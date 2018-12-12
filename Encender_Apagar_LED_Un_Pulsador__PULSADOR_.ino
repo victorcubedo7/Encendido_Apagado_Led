ba9d962cc1c836fa0e71a8c36f184f50770bb606
victorcubedo7 <45770234+victorcubedo7@users.noreply.github.com>
2018-12-12 15:28:31 +0100

    Add files via upload

---
+++ Encender_Apagar_LED_Un_Pulsador__PULSADOR_.ino
@@ -0,0 +1,22 @@
+int led = 9;
+int pulsador = 8;
+int almacen;
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+  pinMode(pulsador, INPUT);
+}
+
+void loop() {
+
+  almacen = digitalRead(pulsador);
+
+  if (almacen == 1) {
+    digitalWrite(led, 1);
+  }
+
+  else {
+    digitalWrite(led, 0);
+  }
+}
